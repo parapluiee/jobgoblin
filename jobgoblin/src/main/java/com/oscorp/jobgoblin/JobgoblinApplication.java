e17ec4d6b6d6506e5dd7eb649c177ef15c91b85b
parpluiee <eli.i.stafford27@gmail.com>
2023-03-18 01:04:34 -0400

    Added spring initializr thing

---
+++ jobgoblin/src/main/java/com/oscorp/jobgoblin/JobgoblinApplication.java
@@ -0,0 +1,13 @@
+package com.oscorp.jobgoblin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobgoblinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobgoblinApplication.class, args);
+	}
+
+}
---
+++ jobgoblin/src/test/java/com/oscorp/jobgoblin/JobgoblinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscorp.jobgoblin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobgoblinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
