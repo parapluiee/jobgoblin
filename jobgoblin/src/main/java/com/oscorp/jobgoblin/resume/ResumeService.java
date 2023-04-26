7e8292c3a8edf9c7d1402ac8eb46d93acf7819f7
mhoney2802 <mkhoneycutt@uncg.edu>
2023-04-25 21:34:05 -0400

    Latest update for JobSeeker UseCases

--- jobgoblin/src/main/java/com/oscorp/jobgoblin/resume/ResumeService.java
+++ jobgoblin/src/main/java/com/oscorp/jobgoblin/resume/ResumeService.java
@@ -1,9 +1,27 @@
 package com.oscorp.jobgoblin.resume;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 /**
  *
  * @author Maximilian
  */
+
+@Service
 public class ResumeService {
-    
+    @Autowired
+    protected ResumeRepository repo;
+    public List<Resume> getAllResumes(){
+        return repo.findAll();
+    }
+    public Object getResumeByJobseekerId(long jobseekerId){
+        return repo.getResumeByJobSeekerId(jobseekerId);
+    }
+    public void saveResume(Resume resume){
+        repo.saveResume(resume);
+    }
+    public void updateResume(Resume resume){
+        repo.updateResume(resume);
+    }
 }
